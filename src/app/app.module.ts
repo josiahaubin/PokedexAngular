5f8a09c4b4176fe9d1fe47ccb2bb51f09b4a61fc
Josiah Aubin <josiahaubin@gmail.com>
2019-11-14 12:35:02 -0700

    added component to home screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonComponent } from './pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonComponent //NOTE this is where you add your new components
   ],
   imports: [
     BrowserModule,
